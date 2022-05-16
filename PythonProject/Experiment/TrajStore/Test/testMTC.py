291b3133344ed044c25b9c3af9edfc9c6817b271
luweir <463047622@qq.com>
2022-05-16 17:17:08 +0800

    1111

--- PythonProject/Experiment/TrajStore/Test/testMTC.py
+++ PythonProject/Experiment/TrajStore/Test/testMTC.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import math
-from Experiment.TrajStore.src.cluster import trajStore_cluster
+from Experiment.TrajStore.src.similarity_based_MTC import mtc
 from Experiment.TrajStore.src.Point import Point
 from Experiment.TrajStore.src.Trajectory import Trajectory
 import random
@@ -10,12 +10,12 @@ import random
 def generate_trajectory(data):
     trajectory = []
     for i in range(len(data)):
-        trajectory.append(Point(data.iloc[i][1] + random.uniform(-0.001, 0.001),
-                                data.iloc[i][2] + random.uniform(-0.001, 0.001),
-                                data.iloc[i][0] + random.randint(-5, 5)))
-        # trajectory.append(Point(data.iloc[i][1],
-        #                         data.iloc[i][2],
-        #                         data.iloc[i][0]))
+        # trajectory.append(Point(data.iloc[i][1] + random.uniform(-0.001, 0.001),
+        #                         data.iloc[i][2] + random.uniform(-0.001, 0.001),
+        #                         data.iloc[i][0] + random.randint(-5, 5)))
+        trajectory.append(Point(data.iloc[i][1],
+                                data.iloc[i][2],
+                                data.iloc[i][0]))
     return trajectory
 
 
@@ -88,35 +88,35 @@ def restore_trajectory_ped_error(trajectory, hash_map):
 
 
 if __name__ == '__main__':
-    data1 = pd.read_csv("../../data/10.9.csv", header=None)[:-15]
+    data1 = pd.read_csv("../../data/10.9.csv", header=None)[:-10]
     # data2 = pd.read_csv("../../data/10.9.csv", header=None)
     # data3 = pd.read_csv("../../data/10.11.csv", header=None)
-    # trajectories = [Trajectory(0, generate_trajectory(pd.read_csv("output_origin_trajectory_0.csv", header=None))),
-    #                 Trajectory(1, generate_trajectory(pd.read_csv("output_origin_trajectory_1.csv", header=None))),
-    #                 Trajectory(2, generate_trajectory(pd.read_csv("output_origin_trajectory_2.csv", header=None))),
-    #                 Trajectory(3, generate_trajectory(pd.read_csv("output_origin_trajectory_3.csv", header=None))),
-    #                 Trajectory(4, generate_trajectory(pd.read_csv("output_origin_trajectory_4.csv", header=None))),
-    #                 Trajectory(5, generate_trajectory(pd.read_csv("output_origin_trajectory_5.csv", header=None))),
-    #                 Trajectory(6, generate_trajectory(pd.read_csv("output_origin_trajectory_6.csv", header=None)))]
-    trajectories = [Trajectory(0, generate_trajectory(data1)),
-                    Trajectory(1, generate_trajectory(data1)),
-                    Trajectory(2, generate_trajectory(data1)),
-                    Trajectory(3, generate_trajectory(data1)),
-                    Trajectory(4, generate_trajectory(data1)),
-                    Trajectory(5, generate_trajectory(data1)),
-                    Trajectory(6, generate_trajectory(data1))]
+    trajectories = [Trajectory(0, generate_trajectory(pd.read_csv("output_origin_trajectory_0.csv", header=None))),
+                    Trajectory(1, generate_trajectory(pd.read_csv("output_origin_trajectory_1.csv", header=None))),
+                    Trajectory(2, generate_trajectory(pd.read_csv("output_origin_trajectory_2.csv", header=None))),
+                    Trajectory(3, generate_trajectory(pd.read_csv("output_origin_trajectory_3.csv", header=None))),
+                    Trajectory(4, generate_trajectory(pd.read_csv("output_origin_trajectory_4.csv", header=None))),
+                    Trajectory(5, generate_trajectory(pd.read_csv("output_origin_trajectory_5.csv", header=None))),
+                    Trajectory(6, generate_trajectory(pd.read_csv("output_origin_trajectory_6.csv", header=None)))]
+    # trajectories = [Trajectory(0, generate_trajectory(data1)),
+    #                 Trajectory(1, generate_trajectory(data1)),
+    #                 Trajectory(2, generate_trajectory(data1)),
+    #                 Trajectory(3, generate_trajectory(data1)),
+    #                 Trajectory(4, generate_trajectory(data1)),
+    #                 Trajectory(5, generate_trajectory(data1)),
+    #                 Trajectory(6, generate_trajectory(data1))]
     print(trajectories)
-    group = trajStore_cluster(trajectories, 1000)
+    group = mtc(trajectories, 500)
     # print(group)
-    # output_origin_trajectory(trajectories)
-    # output_compressed_trajectory(trajectories)
-    # hash_map = {}
-    # for trajectory in trajectories:
-    #     if trajectory.refe_traj_id == -1:
-    #         hash_map[trajectory.traj_id] = trajectory
-    # total_ped = 0
-    # for trajectory in trajectories:
-    #     ped = restore_trajectory_ped_error(trajectory, hash_map)
-    #     total_ped += ped
-    # print("ped error:", total_ped / len(trajectories))
-    # print("max ped error:", max_ped_error)
+    output_origin_trajectory(trajectories)
+    output_compressed_trajectory(trajectories)
+    hash_map = {}
+    for trajectory in trajectories:
+        if trajectory.refe_traj_id == -1:
+            hash_map[trajectory.traj_id] = trajectory
+    total_ped = 0
+    for trajectory in trajectories:
+        ped = restore_trajectory_ped_error(trajectory, hash_map)
+        total_ped += ped
+    print("ped error:", total_ped / len(trajectories))
+    print("max ped error:", max_ped_error)
